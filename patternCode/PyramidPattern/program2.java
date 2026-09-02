67ae3e527bf5e7abbe1665167b79af456ab76bbf
nishantlad-18 <ladnishant07@gmail.com>
2026-09-02 22:16:33 +0530

    Number pyramid pattern

---
+++ patternCode/PyramidPattern/program2.java
@@ -0,0 +1,22 @@
+package LogicBuilding.patternCode.PyramidPattern;
+import java.util.*;
+
+class program2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of row :");
+        int row = sc.nextInt();
+        int cnt = row*2-1;
+
+        for(int i = 1; i <= row; i++){
+            for(int sp = 1; sp < i; sp++){
+                System.out.print("\t");
+            }
+            for(int j = 1; j <= cnt; j++){
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+            cnt -= 2;
+        }
+    }    
+}
