815825107e2c3a9add0e88b91c3b15a91ad93b45
nishantlad-18 <ladnishant07@gmail.com>
2026-09-03 20:57:06 +0530

    pyramid pattern

---
+++ patternCode/PyramidPattern/program4.java
@@ -0,0 +1,28 @@
+package LogicBuilding.patternCode.PyramidPattern;
+import java.util.*;
+
+class pattern19 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a row : ");
+        int row = sc.nextInt();
+
+        for(int i = 1; i <= row; i++){
+            int num =1 ;
+            for(int j = 1; j <= row-i; j++){
+                System.out.print("\t");
+            }
+            for(int k = 1; k <= i*2-1; k++){
+                if(k < i){
+                    System.out.print(num + "\t");
+                    num++;
+                }else{
+                    System.out.print(num + "\t");
+                    num--;
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
+
