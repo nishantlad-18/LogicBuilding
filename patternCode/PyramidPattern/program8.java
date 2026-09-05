fe19d4cb86ded54ee3a1ac605dc114ba3914a404
nishantlad-18 <ladnishant07@gmail.com>
2026-09-05 23:43:17 +0530

    Floyd pyramid

---
+++ patternCode/PyramidPattern/program8.java
@@ -0,0 +1,22 @@
+package LogicBuilding.patternCode.PyramidPattern;
+import java.util.*;
+
+class program8 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of row : ");
+        int row = sc.nextInt();
+        int num = 1;
+        
+        for(int i = 1; i <= row; i++){
+            // for(int sp = 1; sp < i; sp++){
+            //     System.out.print("\t");
+        
+            for(int j = 1; j <= i;j++){
+                System.out.print(num + "\t");
+                num++;
+            }
+            System.out.println();
+        }
+    }        
+}
